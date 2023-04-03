192d0250d2fef34eb7520aa085915483b1c0db41
nRuaif <105905988+nRuaif@users.noreply.github.com>
2023-04-03 10:25:27 +0700

    Build for pip

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import os
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+setup(
+    name=f"GPTQ-for-LLaMa",
+    version=f"1",
+    author="nRuaif",
+    description="GPTQ-for-LLaMa",
+    license="MIT",
+    keywords="gpu optimizers optimization 8-bit quantization compression",
+    url="https://github.com/nRuaif/GPTQ-for-LLaMa",
+    packages=find_packages(),
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+
+)
